33f8a4f0fc68bf992b8b1198812a7322fba70e9b
AlmirMBH <almir.mustafic.tz@gmail.com>
2021-03-10 21:08:52 +0100

    comments deleted from adminmessages

--- adminmessages.php
+++ adminmessages.php
@@ -64,10 +64,8 @@
                                 echo $messages->text . "<br>"; 
                                 echo "<br>";                               
                                 echo $messages->answer ? "<b>Admin's answer</b><br> " . $messages->answer : '';
-                                echo "<br>";
-                                //$messages->answer != '' ? $action = "Edit answer" : $action = "Answer message";
-                                echo "<a href='adminmessages.php?messageId={$messages->id}&action=delete'><b>Delete message</b></a>";
-                                //echo "<a href='usermessages.php?messageId={$messages->id}&action=update'><b>{$action}</b></a>";
+                                echo "<br>";                                
+                                echo "<a href='adminmessages.php?messageId={$messages->id}&action=delete'><b>Delete message</b></a>";                                
                                 echo "</div>";
                                 echo "<br>";                                                            
                             }                            
@@ -76,53 +74,7 @@
                         }
                     ?>
                     
-                </div>
-
-                    <?php                    
-                        // if(login()){
-                        //     if(isset($_GET['messageId']) and isset($_GET['action'])){
-                        //         $messageId = $_GET['messageId'];
-                        //         $action = $_GET['action'];
-                        //         if($action == 'update'){ 
-                        //             $query = "SELECT * FROM messages WHERE id={$messageId}";
-                        //             $result = $connection->query($query);
-                        //             $userMessage = $connection->fetch_object($result);
-                        //             $userId = $userMessage->user_id;
-                        //             $userName = $userMessage->name;
-                        //             $userEmail = $userMessage->email;
-                        //             $adminAnswer = $userMessage->answer;
-                    ?>                                    
-                        <!-- <form action="usermessages.php" method="POST">
-                            <input type="hidden" name="messageId" value="<?php //echo $messageId; ?>">
-                            <input type="hidden" name="userId" value="<?php //echo $userId; ?>">
-                            <input type="hidden" name="name" value="<?php //echo $userName; ?>">
-                            <input type="hidden" name="email" value="<?php //echo $userEmail; ?>">
-                            <textarea name="answer" cols="30" rows="10" placeholder="Enter your answer" required><?php //echo $adminAnswer; ?></textarea><br>
-                            <button name="answer-message">Submit answer</button>
-                        </form> -->
-                                            
-                    <?php 
-                        //         }
-                        //     }
-                        // }       
-                    ?> 
-
-                    <?php
-                        // if(isset($_POST['messageId']) and isset($_POST['userId']) and isset($_POST['name'])and isset($_POST['email']) and isset($_POST['answer'])){
-                        //     $messageId = $_POST['messageId'] . "<br>";
-                        //     $userId = $_POST['userId'] . "<br>";                        
-                        //     $answer = $_POST['answer'];
-
-                        //     $query = "UPDATE messages SET answer='{$answer}' WHERE id='{$messageId}'";
-                        //     $queryExecuted = $connection->query($query);
-
-                        //     if(!$queryExecuted){
-                        //         $message = "Your answer has not been sent!";
-                        //     }else{
-                        //         $message = "Your message has just been sent.";
-                        //     }
-                        // }
-                    ?>    
+                </div>                    
 
                 <div class="user-message">
                     <p style="color:red; font-weight:900; text-align:center"><?php echo $message; ?></p>
